e54139adfa4d5a4b221122318a50d70da195db8c
Thuan Pham <ongeditvideo@gmail.com>
2023-11-16 09:44:34 +0700

    change font, take less space in bottom

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
     server: {
         port: 3000
     },
-    plugins: [react()]
+    plugins: [
+        react(),
+        compression()
+    ]
 })
